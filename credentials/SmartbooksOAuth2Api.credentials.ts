3b0612f3717fe2a2e151dc3693a88e51f1e99d57
Dmitri Tsoy <dmitri.tsoy@smartbooks.ai>
2026-03-05 11:00:02 +0100

    clean up

--- credentials/SmartbooksOAuth2Api.credentials.ts
+++ credentials/SmartbooksOAuth2Api.credentials.ts
@@ -17,44 +17,32 @@ export class SmartbooksOAuth2Api implements ICredentialType {
 
 	properties: INodeProperties[] = [
 		{
-			displayName: 'Environment',
-			name: 'environment',
-			type: 'hidden',
-			default: 'dev',
-			options: [
-				{ name: 'Production', value: 'prod' },
-				{ name: 'Development', value: 'dev' },
-			],
-		},
-
-		// These are used by oAuth2Api internally
-		{
 			displayName: 'Auth URL',
 			name: 'authUrl',
 			type: 'hidden',
 			default:
-				'={{$self.environment === "dev" ? "https://login-dev.smartbooks.ai/authorize" : "https://login.smartbooks.ai/authorize"}}',
+				'https://login.smartbooks.ai/authorize',
 		},
 		{
 			displayName: 'Access Token URL',
 			name: 'accessTokenUrl',
 			type: 'hidden',
 			default:
-				'={{$self.environment === "dev" ? "https://login-dev.smartbooks.ai/oauth/token" : "https://login.smartbooks.ai/oauth/token"}}',
+				'https://login.smartbooks.ai/oauth/token',
 		},
 		{
 			displayName: 'Auth URI Query Parameters',
 			name: 'authQueryParameters',
 			type: 'hidden',
 			default:
-				'={{$self.environment === "dev" ? "audience=https://api-dev.smartbooks.ai/" : "audience=https://api.smartbooks.ai/"}}',
+				'audience=https://api.smartbooks.ai/',
 		},
 		{
 			displayName: 'API Base URL',
 			name: 'baseURL',
 			type: 'hidden',
 			default:
-				'={{$self.environment === "dev" ? "https://app-dev.smartbooks.ai" : "https://app.smartbooks.ai"}}',
+				'https://app.smartbooks.ai',
 		},
 		{
 			displayName: 'Scope',
@@ -73,7 +61,6 @@ export class SmartbooksOAuth2Api implements ICredentialType {
 			name: 'allowedHttpRequestDomains',
 			type: 'hidden',
 			default: 'all',
-
 		}
 	];
 }
